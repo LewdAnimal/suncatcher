0eafb48f558ff68a6475c04941197a727005193b
MadColor <info@madcolor.ru>
2023-01-22 11:02:41 +0500

    Активные элементы и различные мелочи

--- js/script.js
+++ js/script.js
@@ -10,6 +10,46 @@ const swiper = new Swiper('.slider', {
 });
 
 
+let
+  burger = document.querySelector('.burger'),
+  mobile_menu = document.querySelector('.mobile-menu');
+
+burger.onclick = function () {
+  this.classList.toggle("burger--open");
+
+  if (this.classList.contains("burger--open")) {
+    mobile_menu.style.display = 'block';
+    document.body.style.overflow = 'hidden';
+  }
+  else {
+    mobile_menu.style.display = 'none';
+    document.body.style.overflow = 'visible';
+  }
+}
+
+
+
+let
+  order_buttom = document.querySelectorAll('.order-button'),
+  order_window = document.querySelector('.order-window'),
+  overlay = document.createElement('div');
+
+order_buttom.forEach(function (item) {
+
+  item.onclick = function () {
+    overlay.className = 'overlay';
+    document.body.prepend(overlay);
+    order_window.style.display = 'block';
+  }
+});
+
+
+overlay.onclick = function () {
+  order_window.style.display = 'none';
+  document.body.removeChild(overlay);
+}
+
+
 const project_slider = new Swiper('.project-slider__items', {
   slidesPerView: "auto",
   spaceBetween: 40,
